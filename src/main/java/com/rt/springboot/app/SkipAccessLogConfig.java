d6de06dda7f8174cd6b2ea335f8a336957e30c13
guyriddler <guyriddler@gmail.com>
2025-10-28 11:06:47 -0500

    Added skip access log config

---
+++ src/main/java/com/rt/springboot/app/SkipAccessLogConfig.java
@@ -0,0 +1,27 @@
+package com.rt.springboot.app;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/actuator/*")
+public class SkipAccessLogConfig implements Filter {
+
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                throws IOException, ServletException {
+
+            String path = ((HttpServletRequest) request).getRequestURI();
+
+            // Skip logging for prometheus, metrics, or health endpoints
+            if (path.matches(".*/actuator/(prometheus|metrics|health).*")) {
+                request.setAttribute("skipAccessLog", Boolean.TRUE);
+            }
+
+            chain.doFilter(request, response);
+        }
+    }
+
+
